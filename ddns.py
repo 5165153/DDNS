64d373d1d2f33c9f44fc363dc77b2b4536c2e930
Sky <ssssss11099@126.com>
2024-01-29 21:12:34 +0800

    把dns记录与本地ip的一致性修改为更新ddns解析的依据,并且增强了ha

--- ddns.py
+++ ddns.py
@@ -1,6 +1,6 @@
-#安装以下sdk
-#pip install aliyun-python-sdk-core
-#pip install aliyun-python-sdk-alidns
+# 安装以下sdk
+# pip install aliyun-python-sdk-core
+# pip install aliyun-python-sdk-alidns
 
 import telnetlib
 import re
@@ -9,11 +9,20 @@ from time import sleep
 from datetime import datetime
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.request import CommonRequest
+from aliyunsdkalidns.request.v20150109.DescribeDomainRecordInfoRequest import DescribeDomainRecordInfoRequest
 import configparser
 import os
 import traceback
+import json
 
-#sleep(100) # 不要让用户觉得启动的太快
+# 获取当前格式化时间的函数
+def get_current_formatted_time():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+# 带时间戳的消息打印函数
+def print_timestamped_message(message):
+    current_time = get_current_formatted_time()
+    print(f"[{current_time}] {message}")
 
 # 创建一个示例配置文件的函数
 def create_sample_config_file(path):
@@ -30,7 +39,8 @@ def create_sample_config_file(path):
         'DOMAIN': 'example.com',
         'RECORD_ID': 'record_id',
         'Record_name': 'www',
-        'Retry_Interval': '5'
+        'Retry_Interval': '5',
+        'Mismatch_Threshold': '3'
     }
     with open(path, 'w') as configfile:
         config.write(configfile)
@@ -42,7 +52,7 @@ config_file_path = os.path.join(script_dir, 'config.ini')
 # 检查配置文件是否存在
 if not os.path.exists(config_file_path):
     create_sample_config_file(config_file_path)
-    logging.error(f"未找到配置文件。已创建一个示例配置文件位于 {config_file_path}。请根据你的设置更改它。")
+    print_timestamped_message(f"未找到配置文件,已创建一个示例配置文件位于 {config_file_path},请根据你的设置更改它")
     exit(1)
 
 config = configparser.ConfigParser()
@@ -58,16 +68,11 @@ logging.basicConfig(
 
 # 获取带重试机制的PPPoE IP地址的函数
 def get_pppoe_ip(device_config):
-    #print("Debug: get_pppoe_ip called with device_config:", device_config)
-
     max_retries = 5
     retry_interval = int(device_config['retry_interval'])
-    #print("Debug: retry_interval set to", retry_interval)
 
     for attempt in range(max_retries):
         try:
-            device_ip = device_config['device_ip']
-            #print("Debug: device_ip set to", device_ip)
             with telnetlib.Telnet(device_config['device_ip'], int(device_config['telnet_port']), timeout=10) as telnet:
                 # Telnet操作代码
                 telnet.read_until(b"Username:")
@@ -85,7 +90,6 @@ def get_pppoe_ip(device_config):
                     return ip_address_match.group(1)
             return "IP未找到"
         except Exception as e:
-            #print("Error: Configuration key not found:", e)
             logging.error(f"在 get_pppoe_ip 中尝试 {attempt + 1} 失败: {str(e)}")
             sleep(retry_interval)
     return None
@@ -115,62 +119,98 @@ def update_dns_record(ip, device_config):
             sleep(retry_interval)
     return None
 
-# 获取当前格式化时间的函数
-def get_current_formatted_time():
-    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#  带重试机制的获取Aliyun DNS记录对应的IP
+def get_current_dns_ip(access_key, accesskey_secret, record_id, retry_interval):
+    client = AcsClient(access_key, accesskey_secret, 'cn-hangzhou')
+    request = DescribeDomainRecordInfoRequest()
+    request.set_accept_format('json')
+    request.set_RecordId(record_id)
+
+    attempt = 0
+    max_retries = 5  # 根据需要调整最大重试次数
+
+    while attempt < max_retries:
+        try:
+            response = client.do_action_with_exception(request)
+            response_dict = json.loads(response)
+            if 'Value' in response_dict:
+                print_timestamped_message(f'查询DNS记录成功')
+                logging.info(f'查询DNS记录成功,ip:{response_dict}')
+                return response_dict['Value']  # 返回记录的IP
+            else:
+                print_timestamped_message("查询DNS记录成功,但未找到'Value'键,检查record_id是否正确")
+                logging.error("查询DNS记录成功,但未找到'Value'键,检查record_id是否正确")
+                return None
+        except Exception as e:
+            print_timestamped_message("查询DNS记录失败")
+            logging.error(f"查询DNS记录失败: {str(e)}")
+            attempt += 1
+            if attempt < max_retries:
+                sleep(retry_interval)  # 重试间隔
+
+    logging.error(f"查询DNS记录失败: 达到最大重试次数 {max_retries}")
+    return None
+
+mismatch_count = {}
 
 # 单个设备的主要函数
-def main(device_config, last_ip):
-    #print("当前设备配置:", device_config)  # 检查配置内容
-    try:
-        current_time = get_current_formatted_time()
-        current_ip = get_pppoe_ip(device_config)
-        if current_ip and current_ip != "IP未找到" and current_ip != last_ip:
-            # IP地址发生了变化，执行DDNS更新
-            response = update_dns_record(current_ip, device_config)
-            if response:
-                logging.info(f"已更新DNS记录,将 {device_config['record_name']} 更新为IP {current_ip}")
-                print(f"[{current_time}] 已更新DNS记录,将 {device_config['record_name']} 更新为IP {current_ip}")
-            else:
-                logging.error("更新DNS记录失败。")
-                print(f"[{current_time}] 更新DNS记录失败。")
-            last_ip = current_ip  # 更新last_ip
-        elif current_ip and current_ip != "IP未找到":
-            # IP地址未发生变化，记录日志
-            logging.info(f"{device_config['record_name']} 的IP地址未发生变化")
-        else:
-            # 无法获取IP地址
-            logging.error("无法获取当前IP。")
-    except Exception as e:
-        current_time = get_current_formatted_time()
-        error_message = f"[{current_time}] 在 main 中发生错误: {str(e)}"
-        logging.error(error_message)
-        print(error_message)
-        traceback.print_exc()
-    return last_ip  # 返回更新后的last_ip
+def main(device_config, section):
+    global mismatch_counts
+    mismatch_threshold = int(device_config.get('mismatch_threshold', 3))  # 默认值为3
+    retry_interval = int(device_config.get('retry_interval', 5))  # 从配置读取重试间隔，默认为5秒
 
-# 循环运行每个设备的主要函数
+    # 初始化当前设备的不匹配计数
+    if section not in mismatch_counts:
+        mismatch_counts[section] = 0
+
+    try:
+        current_dns_ip = get_current_dns_ip(device_config['access_key'], device_config['accesskey_secret'], device_config['record_id'], retry_interval)
+        current_pppoe_ip = get_pppoe_ip(device_config)
+
+        if current_pppoe_ip and current_pppoe_ip != "IP未找到":
+            if current_pppoe_ip != current_dns_ip:
+                mismatch_counts[section] += 1  # 增加不匹配计数
+                print_timestamped_message(f"{section} 不匹配次数: {mismatch_counts[section]}")
+                if mismatch_counts[section] >= mismatch_threshold:
+                    # 连续不匹配超过阈值，更新DNS记录
+                    response = update_dns_record(current_pppoe_ip, device_config)
+                    if response:
+                        logging.info(f"{section} 由于连续不匹配, 已更新DNS记录, 将 {device_config['record_name']} 更新为IP {current_pppoe_ip}")
+                        print_timestamped_message(f"{section} 由于连续不匹配, 已更新DNS记录, 将 {device_config['record_name']} 更新为IP {current_pppoe_ip}")
+                        mismatch_counts[section] = 0  # 成功更新后重置不匹配计数
+            else:
+                # IP地址与DNS记录一致，重置不匹配计数器
+                mismatch_counts[section] = 0
+        else:
+            # 无法获取PPPoE IP地址
+            logging.error(f"{section} 无法获取当前PPPoE IP。")
+    except Exception as e:
+        error_message = f"{section} 在 main 中发生错误: {str(e)}"
+        logging.error(error_message)
+        print_timestamped_message(error_message)
+        traceback.print_exc()
+
+# 主循环
 if __name__ == "__main__":
-    last_ip = {}  # 初始化last_ip字典
     device_processed = {}  # 初始化设备处理标记字典
 
     while True:
         for section in config.sections():
             device_config = dict(config.items(section))
-            current_time = get_current_formatted_time()
-            print(f"[{current_time}] 正在运行 {section}...")
+            print_timestamped_message(f"正在运行 {section}...") # 表示目前代码运行正常
 
             # 检查设备是否已处理过
             if device_processed.get(section, False):
                 # 如已处理过进行延迟
-                sleep(int(device_config['time']))
+                sleep_time = int(device_config.get('time', 60))  # 使用设备配置中的时间，或默认为60秒
+                print_timestamped_message(f"等待 {sleep_time} 秒后再次处理 {section}")
+                sleep(sleep_time)
 
             try:
-                # 处理设备
-                last_ip[section] = main(device_config, last_ip.get(section))
+                main(device_config, section)
             except Exception as e:
-                error_message = f"[{current_time}] 处理 {section} 时发生错误: {e}"
-                print(error_message)
+                error_message = f"处理 {section} 时发生错误: {e}"
+                print_timestamped_message(error_message)
                 logging.error(error_message)
 
             # 标记设备已处理
